f64f0205f0d4e12e85a0895365d18a8f3d49c0ec
Puls Sovi <pulsovi@outlook.com>
2018-07-09 16:56:20 +0200

    Premiere version fonctionnelle

---
+++ ActiveWindow.ahk
@@ -0,0 +1,1 @@
+WinActivate, ahk_pid %1%
--- firefoxProfile.bat
+++ firefoxProfile.bat
@@ -1,23 +1,22 @@
-::@ECHO off
-REM "C:\Program Files\Mozilla Firefox\firefox.exe" -P "David" -no-remote
+@ECHO off
 SET firefoxOpened=0
-FOR /F "delimiters=" %%i IN ('TASKLIST ^| FIND "firefox"') DO SET firefoxOpened=1
-IF %firefoxOpened% EQU 1 (
-	::firefox is run
-	SET profileOk=0
-	FOR /F "delimiters=" %%j IN ('WMIC PROCESS GET commandline ^| FIND "firefox.exe""  -P ""%1"""') DO (
-		SET profileOk=1
-		ECHO %%j
-	)
-	IF %%profileOk%% EQU 1 (
-		::firefox profile is run
-		echo ok
-	) ELSE (
-		::firefox is run on another profile
-		echo ko
-		::START "Firefox %1" "C:\Program Files\Mozilla Firefox\firefox.exe" -P "%1" -no-remote
-	)
-) ELSE (
-	::firefox not run
+FOR /F "delims=" %%i IN ('TASKLIST ^| FIND "firefox"') DO SET firefoxOpened=1
+IF %firefoxOpened% EQU 0 (
 	START "Firefox %1" "C:\Program Files\Mozilla Firefox\firefox.exe" -P "%1"
+	GOTO :EOF
+)
+
+SET profileOk=0
+FOR /F "delims=" %%j IN ('WMIC PROCESS GET ProcessId^,CommandLine ^| FIND "firefox.exe""  -P ""%1"""') DO (
+	SET profileOk=1
+	SET CommandLineTampon=%%j
+)
+IF %profileOk% EQU 0 (
+	START "Firefox %1" "C:\Program Files\Mozilla Firefox\firefox.exe" -P "%1" --no-remote
+	GOTO :EOF
+)
+
+FOR /F "tokens=6" %%k IN ("%CommandLineTampon%") DO SET pidFfPf=%%k
+IF %profileOk% EQU 1 (
+	START ActiveWindow.ahk %pidFfPf%
 )
